f761a93402c7fec64496ef7cc73fcbc3ca1391a4
Nikto <nzenn9@gmail.com>
2024-10-02 11:35:47 +0300

    ready

--- main.py
+++ main.py
@@ -2,38 +2,68 @@ import asyncio
 import logging
 import uvicorn
 from aiogram import Bot, Dispatcher
+from aiogram.types import Update
+from fastapi import Request
 from middlewares.db import DataBaseSession
 from database.engine import create_db, session_maker
 from database.orm_query import orm_del_invoices
 from handlers import router
-from data.config import bot, app  # FastAPI импортирован как `app`
-
-async def on_startup(bot):
-    # Инициализация базы данных или других ресурсов
-    await create_db()
+from data.config import bot, app, URL_APP
 
 
-async def on_shutdown(bot):
-    # Очистка ресурсов перед завершением
+
+@app.post('/webhook')
+async def handle_webhook(request: Request):
+    """Обработка вебхуков от Telegram."""
+    data = await request.json()
+    update = Update(**data)
+
+    # Обработка обновления через диспетчер
+    await dp._process_update(bot, update)
+
+    return {"status": "ok"}
+
+async def set_webhook():
+    """Установка вебхука вручную при запуске приложения."""
+    webhook_set = await bot.set_webhook(URL_APP + '/webhook')
+    if webhook_set:
+        print("Webhook установлен успешно")
+    else:
+        print("Ошибка установки webhook")
+
+
+async def on_startup():
+    """Инициализация при старте: установка вебхука и инициализация базы данных."""
+    await create_db()  # Инициализация базы данных
+    await set_webhook()
+
+
+async def on_shutdown():
+    """Очистка ресурсов перед завершением работы."""
     async with session_maker() as session:
         await orm_del_invoices(session, bot)
+
+    # Удаление вебхука
+    await bot.delete_webhook()
     print('бот лег')
 
 
 async def run_bot():
-    """Запуск Telegram-бота."""
-    bot.my_admins_list = [877804669]
+    """Запуск Telegram-бота с вебхуками."""
+    global dp
+    bot.my_admins_list = [877804669]  # Список администраторов
     dp = Dispatcher(bot=bot)
     dp.include_router(router)
 
-    dp.startup.register(on_startup)
+    # Регистрация событий завершения работы
     dp.shutdown.register(on_shutdown)
 
+    # Применение middleware для работы с сессией базы данных
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
 
     logging.basicConfig(level=logging.INFO)
 
-    await dp.start_polling(bot)
+    # Не запускаем polling, так как используется вебхук
 
 
 async def run_fastapi():
@@ -44,7 +74,8 @@ async def run_fastapi():
 
 
 async def main():
-    # Запускаем бота и сервер FastAPI параллельно
+    """Запускаем бота, вебхуки и сервер FastAPI параллельно."""
+    await on_startup()  # Вызов on_startup для установки вебхуков перед запуском
     await asyncio.gather(
         run_bot(),
         run_fastapi()
